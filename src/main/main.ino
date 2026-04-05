a41a01f3668539c3e7b25f9cbc06b621b0ba9fd2
Sebastian42-5 <sebassotosandrea@gmail.com>
2026-04-04 23:09:46 -0400

    added new stl files

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,8 +1,12 @@
+#include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
 #include <splash.h>
 #include <HCSR04.h>
 #include <FluxGarage_RoboEyes.h>
+
+
 
 int clawPin = 3;
 int tailPin = 4;
@@ -12,8 +16,8 @@ int in2 = 7;
 int in3 = 8;
 int in4 = 9;
 int enb = 10;
-int echo = 11;
-int trig = 12;
+int echoPin = 11;
+int trigPin = 12;
 int touchSensor = 13; 
 
 Servo tail;
@@ -35,47 +39,59 @@ void setup() {
 
   Serial.begin(9600);
 
-  claw.attach(claw);
-  tail.attach(tail);
+  Serial.println("hello");
+
+  claw.attach(clawPin);
+  tail.attach(tailPin);
 
   pinMode(ena, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  pinMode(touchSensor, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(touchSensor, INPUT_PULLUP);
 
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(ena, 120);
-  analogWrite(enb, 120);
-  delay(2000);
+  // digitalWrite(in1, HIGH);
+  // digitalWrite(in2, LOW);
+  // digitalWrite(in3, HIGH);
+  // digitalWrite(in4, LOW);
+  // analogWrite(ena, 120);
+  // analogWrite(enb, 120);
+  // delay(2000);
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(2000);
+  // digitalWrite(in1, LOW);
+  // digitalWrite(in2, LOW);
+  // digitalWrite(in3, LOW);
+  // digitalWrite(in4, LOW);
+  // delay(2000);
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(ena, 120);
-  analogWrite(enb, 120);
-  delay(2000);
+  // digitalWrite(in1, LOW);
+  // digitalWrite(in2, HIGH);
+  // digitalWrite(in3, LOW);
+  // digitalWrite(in4, HIGH);
+  // analogWrite(ena, 120);
+  // analogWrite(enb, 120);
+  // delay(2000);
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(2000);
+  // digitalWrite(in1, LOW);
+  // digitalWrite(in2, LOW);
+  // digitalWrite(in3, LOW);
+  // digitalWrite(in4, LOW);
+  // delay(2000);
 
-  roboEyes.begin(SCREEN_WIDTH, SCREEN_HEIGHT, 100);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+    Serial.println(F("Serial allocation failed"));
+    for(;;);
+  }
+
+  if(display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+    Serial.println("screen initialized");
+  }
+
+  roboEyes.begin(&display, SCREEN_WIDTH, SCREEN_HEIGHT, 120);
+  roboEyes.open();
   roboEyes.setAutoblinker(ON, 3, 2);
   roboEyes.setIdleMode(ON, 2, 2);
   roboEyes.setWidth(80, 36);
@@ -83,6 +99,7 @@ void setup() {
   roboEyes.setBorderradius(4, 8); 
   roboEyes.setSpacebetween(1); 
   roboEyes.setCyclops(ON);
+  roboEyes.setMood(DEFAULT);
 
 }
 
@@ -91,8 +108,9 @@ void loop() {
   // if(Serial.available()){
   //   String data = Serial.readStringUntil("\n");
   // }
-  roboEyes.setMood(DEFAULT);
-  touchDetected = digitalRead(tocuhSensor);
+  roboEyes.update();
+  int touchDetected = digitalRead(touchSensor);
+  // Serial.println(touchDetected);
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -103,15 +121,16 @@ void loop() {
 
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
-  if(touchDetected){
+  if(touchDetected == LOW){
+    Serial.println("happy mode");
     roboEyes.setMood(HAPPY);
-    for(int pos = 0; pos < 30; pos += 1){
+    for(int pos = 0; pos < 30; pos += 3){
       tail.write(pos);
-      delay(100);
+      delay(20);
     }
-    for(int pos = 30; pos > 0; pos -= 1){
+    for(int pos = 30; pos > 0; pos -= 3){
       tail.write(pos);
-      delay(100);
+      delay(20);
     }
   }
 
