a2c94b27ea57121c6166e61b4119bd4af1020aa9
Sebastian42-5 <sebassotosandrea@gmail.com>
2026-07-14 21:33:59 -0400

    adding volume mixer code

--- src/drive_test/drive_test.ino
+++ src/drive_test/drive_test.ino
@@ -1,9 +1,9 @@
-int ena = 5; 
+int ena = 10; 
 int in1 = 6;
 int in2 = 7;
 int in3 = 8;
 int in4 = 9;
-int enb = 10;
+int enb = 5;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,8 +16,8 @@ void setup() {
 
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
   analogWrite(ena, 120);
   analogWrite(enb, 120);
   delay(2000);
--- src/music.py
+++ src/music.py
@@ -1,19 +1,29 @@
 import serial
-import pyvolume
 import time
+from pycaw.pycaw import AudioUtilities
+
+
 
 serial = serial.Serial(port="COM6", baudrate=9600, timeout=0.1)
 
+def set_volume(percentage):
+   
+    device = AudioUtilities.GetSpeakers()
+    volume = device.EndpointVolume
+    
+    volume.SetMasterVolumeLevelScalar(percentage / 100, None)
+    print(f"Volume set to {percentage}%")
 
-while True:
-    packet = serial.readline()
-    if packet:
-        message = packet.decode('utf-8').strip()
-        if message:
-            print(message)
-            desired_message = float(message)
-            volume = int((100 * desired_message) / 675)
-            pyvolume.custom(percent=30)
+if __name__ == "__main__":
+    while True:
+        packet = serial.readline()
+        if packet:
+            message = packet.decode('utf-8').strip()
+            if message:
+                print(message)
+                desired_message = float(message)
+                volume = int((100 * desired_message) / 675)
+                set_volume(volume)
 
 
 
