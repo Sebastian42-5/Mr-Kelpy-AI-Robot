3cead090313a6c58e7b40b21aa6dd4b5344d52a2
Sebastian42-5 <sebassotosandrea@gmail.com>
2026-09-22 22:53:31 -0400

    started file for pytorch keyword training

---
+++ keyword_training.py
@@ -0,0 +1,10 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import torchaudio
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+from tqdm.notebook import tqdm
+
